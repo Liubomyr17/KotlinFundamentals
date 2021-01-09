4bfa2cd06723cd4783845c021c954debbdd548fc
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2021-01-09 17:56:28 +0200

    Comparison operators

--- app/src/main/java/com/example/kotlinfundamentals/KotlinBasics.kt
+++ app/src/main/java/com/example/kotlinfundamentals/KotlinBasics.kt
@@ -8,6 +8,16 @@ fun main() {
     val b = 3
     var resultDouble : Double
     resultDouble = a / b
-    println("Result: $resultDouble")
+//    println("Result: $resultDouble")
     // Result: 1.6666666666666667
+
+    // Comparison operators (==, !=, <, >, <=, =>)
+    val isEqual = 5 == 3
+    println("isEqual is $isEqual")
+    val isNotEqual = 5 != 5
+    println("isNotEqual is $isNotEqual")
+    println("is5greater3 ${5 > 3}")
+    println("is5LowerEqual3 ${5 <= 3}")
+//    is5greater3 true
+//    is5LowerEqual3 false
 }
