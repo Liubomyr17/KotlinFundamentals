e66204fc21cf6c1ee4959e6d28d7185fc6b06476
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2021-01-09 17:39:59 +0200

    4. Arithmetic Operators

--- app/src/main/java/com/example/kotlinfundamentals/KotlinBasics.kt
+++ app/src/main/java/com/example/kotlinfundamentals/KotlinBasics.kt
@@ -1,36 +1,13 @@
 package com.example.kotlinfundamentals
 
 fun main() {
-    // type String
-    val myName = "Leo"
-    // type int (32 bits of memory)
-    val myAge = 31
 
-    val myByte: Byte = 13 // 8 bits
-    val myShort: Short = 125 // 16 bits
-    val myInt: Int = 123123123 // 32 bits
-    val myLong: Long = 12_039_812_309 // 64 bits
-
-    val myFloat: Float = 13.37F // 32 bits
-    val myDouble = 3.14151236987 // 64 bits
-
-    // Booleans
-    var isSunny: Boolean = true
-    isSunny = false
-
-    // Characters
-    val letterChar = 'A'
-    val digitChar = '$'
-
-    // Strings
-    val myStr = "Hello World"
-    val firstCharInStr = myStr[0]
-    val lastCharInString = myStr[myStr.length - 1]
-
-    println("First character + $firstCharInStr")
-    println("First character + $lastCharInString")
-
-
-
-
+    // Arithmetic operators (+, -, *, /, %)
+    var result = 5 + 3
+    val a = 5.0
+    val b = 3
+    var resultDouble : Double
+    resultDouble = a / b
+    println("Result: $resultDouble")
+    // Result: 1.6666666666666667
 }
