700a92fe226cb834073aa80688dcb2a3fa92ce1d
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2021-01-09 15:29:46 +0200

    2. Datatypes Variables

--- app/src/main/java/com/example/kotlinfundamentals/KotlinBasics.kt
+++ app/src/main/java/com/example/kotlinfundamentals/KotlinBasics.kt
@@ -1,15 +1,20 @@
 package com.example.kotlinfundamentals
 
 fun main() {
-    var myName = "Leo"
+    // type String
+    val myName = "Leo"
+    // type int (32 bits of memory)
+    val myAge = 31
+
+    val myByte: Byte = 13 // 8 bits
+    val myShort: Short = 125 // 16 bits
+    val myInt: Int = 123123123 // 32 bits
+    val myLong: Long = 12_039_812_309 // 64 bits
+
+    val myFloat: Float = 13.37F // 32 bits
+    val myDouble = 3.14151236987 // 64 bits
+
     println("Hello " + myName)
-    println("Hello $myName")
 
-    // Difference between VAR and VAL
-    myName = "Francis"
-    println("Hello $myName")
-
-//    val myName = "Frank" --> error VAR is mutable / VAL is immutable
-//    println("Hello $myName")
 
 }
