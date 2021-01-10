ac18f9c369a47305b29b7ffc5270d62859605435
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2021-01-10 12:16:39 +0200

    6. Assignement, Increment And Decrement Operators

--- app/src/main/java/com/example/kotlinfundamentals/KotlinBasics.kt
+++ app/src/main/java/com/example/kotlinfundamentals/KotlinBasics.kt
@@ -1,23 +1,22 @@
 package com.example.kotlinfundamentals
 
 fun main() {
+    var myNum = 5
+    myNum += 3
+    //println("myNum is $myNum")
+    //myNum is 8
+    myNum *= 4
+    //println("myNum is $myNum")
+    // myNum is 32
+    // Increment & Decrement operators (++, --)
+    myNum++
+    println("myNum is $myNum")
+    println("myNum is ${myNum++}")
+    println("myNum is ${++myNum}")
+    println("myNum is ${--myNum}")
+//    myNum is 33
+//    myNum is 33
+//    myNum is 35
+//    myNum is 34
 
-    // Arithmetic operators (+, -, *, /, %)
-    var result = 5 + 3
-    val a = 5.0
-    val b = 3
-    var resultDouble : Double
-    resultDouble = a / b
-//    println("Result: $resultDouble")
-    // Result: 1.6666666666666667
-
-    // Comparison operators (==, !=, <, >, <=, =>)
-    val isEqual = 5 == 3
-    println("isEqual is $isEqual")
-    val isNotEqual = 5 != 5
-    println("isNotEqual is $isNotEqual")
-    println("is5greater3 ${5 > 3}")
-    println("is5LowerEqual3 ${5 <= 3}")
-//    is5greater3 true
-//    is5LowerEqual3 false
 }
