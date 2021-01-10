28eb964b23a03d97036b8166db3759b9f8deee1f
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2021-01-10 12:45:42 +0200

    7. If Statements

--- app/src/main/java/com/example/kotlinfundamentals/KotlinBasics.kt
+++ app/src/main/java/com/example/kotlinfundamentals/KotlinBasics.kt
@@ -1,22 +1,25 @@
 package com.example.kotlinfundamentals
 
 fun main() {
-    var myNum = 5
-    myNum += 3
-    //println("myNum is $myNum")
-    //myNum is 8
-    myNum *= 4
-    //println("myNum is $myNum")
-    // myNum is 32
-    // Increment & Decrement operators (++, --)
-    myNum++
-    println("myNum is $myNum")
-    println("myNum is ${myNum++}")
-    println("myNum is ${++myNum}")
-    println("myNum is ${--myNum}")
-//    myNum is 33
-//    myNum is 33
-//    myNum is 35
-//    myNum is 34
+    var heightPerson1 = 170
+    var heightPerson2 = 189
+
+    if (heightPerson1 > heightPerson2) {
+        println("use raw force")
+    } else if (heightPerson1 == heightPerson2) {
+        println("use 1337")
+    } else {
+        println("use technique")
+    }
+    var age = 17
+    if (age >= 21) {
+        println("Now you may drink in the US")
+    } else if (age >= 18) {
+        println("You may vote now")
+    } else if (age >= 16) {
+        println("You may drive now")
+    } else {
+        println("You are too young")
+    }
 
 }
